6ccad74ebab8c00c0eb4d7558b593a5414e89f12
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-07-26 23:38:44 +0700

    Initial Commit

---
+++ google-test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  google-test
+//
+//  Created by wk on 7/26/2557 BE.
+//  Copyright (c) 2557 wk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
